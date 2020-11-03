e8ff4729ec77696cfa08012455ae6097e702169b
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-03 20:44:11 +0900

    Instuctorクラスの作成

---
+++ lib/models/instructor.dart
@@ -0,0 +1,31 @@
+
+class Instuctor {
+  String instructorName;
+  String rating;
+  String instructorPic;
+
+  Instuctor({this.instructorName, this.rating, this.instructorPic});
+}
+
+var allInstructors = [
+  Instuctor(
+    instructorName: 'Cora Wong',
+    rating: '9.95',
+    instructorPic: 'assets/a.png',
+  ),
+  Instuctor(
+    instructorName: 'Cora Wong',
+    rating: '9.95',
+    instructorPic: 'assets/b.png',
+  ),
+  Instuctor(
+    instructorName: 'Cora Wong',
+    rating: '9.95',
+    instructorPic: 'assets/c.png',
+  ),
+  Instuctor(
+    instructorName: 'Cora Wong',
+    rating: '9.95',
+    instructorPic: 'assets/d.png',
+  ),
+];
