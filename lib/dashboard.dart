b7e5cc6aa70fefa255e8d11634fa97bbae1ea76e
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-08 16:52:11 +0900

    新規ファイルの追加

---
+++ lib/dashboard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class DashbordPage extends StatefulWidget {
+  @override
+  _DashbordPageState createState() => _DashbordPageState();
+}
+
+class _DashbordPageState extends State<DashbordPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
