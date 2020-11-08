16b258533707208bdbb28cc243c4366d940ca490
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-08 17:16:52 +0900

    背景ピンクのfilterアイコンを追加

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class DashbordPage extends StatefulWidget {
   @override
@@ -8,6 +9,29 @@ class DashbordPage extends StatefulWidget {
 class _DashbordPageState extends State<DashbordPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 20.0),
+        Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: 40.0,
+                width: 40.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7.0),
+                    color: Color(0xFFFD4F99)
+                  ),
+                child: Center(
+                  child: Icon(Icons.filter_list, color: Colors.white),
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
