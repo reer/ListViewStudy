d6fa91b04a97e8c41e6d4d48a14f8b323eb7f331
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-08 19:08:50 +0900

    画像表示

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -50,8 +50,88 @@ class _DashbordPageState extends State<DashbordPage> {
               ),
             ],
           ),
+        ),
+        SizedBox(height: 5.0),
+        Container(
+          height: MediaQuery.of(context).size.height - 150.0,
+          child: ListView(
+            children: <Widget>[
+              _buildListItem('assets/images/jp3.png', 'japan', 'Explore tha land of the rising night')
+            ],
+          ),
         )
       ],
+    );
+  }
+
+  _buildListItem(String imgPath, String country, String description){
+    return Padding(
+      padding: EdgeInsets.all(15.0),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: 300.0,
+          ),
+          Container(
+            height: 300.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              image: DecorationImage(
+                image: AssetImage(imgPath),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.6), BlendMode.darken)
+              )
+            ),
+          ),
+          Container(
+            height: 300.0,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    country,
+                    style: GoogleFonts.montserrat(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      textStyle: TextStyle(color: Colors.white)
+                    ),
+                  ),
+                  Text(
+                    description,
+                    style: GoogleFonts.montserrat(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w400,
+                        textStyle: TextStyle(color: Colors.white)
+                    ),
+                  ),
+                  SizedBox(height: 20.0,),
+                  InkWell(
+                    onTap: (){},
+                    child: Container(
+                      height: 50.0,
+                      width: 125.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.white
+                      ),
+                      child: Center(
+                        child: Text('Explore now',
+                        style: GoogleFonts.montserrat(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold,
+                          textStyle: TextStyle(color: Color(0xFFFD4F99))
+                        ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
