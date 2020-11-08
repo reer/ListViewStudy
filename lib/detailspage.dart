77d51f674681b429b5b3ab3fa4007438cef289d8
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-08 19:41:03 +0900

    詳細画面の追加

---
+++ lib/detailspage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatefulWidget {
+
+  final imgPath, title;
+
+  DetailsPage({this.imgPath, this.title});
+
+  @override
+  _DetailsPageState createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  widget.imgPath), fit: BoxFit.cover
+              )
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
