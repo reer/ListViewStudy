58841bfb196e4ab4ec2756ab0f1c8d397c0ee87e
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-08 17:23:13 +0900

    ヘッダー部分のデザイン完了

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DashbordPage extends StatefulWidget {
   @override
@@ -27,7 +28,26 @@ class _DashbordPageState extends State<DashbordPage> {
                 child: Center(
                   child: Icon(Icons.filter_list, color: Colors.white),
                 ),
-              )
+              ),
+              Text(
+                'HOME',
+                style: GoogleFonts.montserrat(
+                    fontSize: 16.5,
+                    fontWeight: FontWeight.w400,
+                    textStyle: TextStyle(color: Colors.white)
+                ),
+              ),
+              Container(
+                height: 40.0,
+                width: 40.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7.0),
+                    color: Color(0xFF353535)
+                ),
+                child: Center(
+                  child: Icon(Icons.bookmark_border, color: Colors.white),
+                ),
+              ),
             ],
           ),
         )
