d8751fb8f08bce0ee356c4953ee4f73aa563c393
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-08 15:45:56 +0900

    ファイル先の変更

--- lib/instructor/list_instructor.dart
+++ lib/instructor/list_instructor.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_list_app/instructor_detail.dart';
+import 'package:flutter_list_app/instructor/instructor_detail.dart';
 import 'package:flutter_list_app/models/instructor.dart';
 import 'package:google_fonts/google_fonts.dart';
 
