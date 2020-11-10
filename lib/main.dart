3e81cc6287a13b0c2b75fdb146493794e5f63cc4
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-11 00:11:28 +0900

    BottomNavigationBarのファイルとして再構築

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_list_app/card_list.dart';
 
 import 'buttomNavigation.dart';
+import 'buttomView/home.dart';
+import 'buttomView/setting.dart';
 import 'instructor/list_instructor.dart';
 import 'novel_design.dart';
 
@@ -15,10 +18,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      // theme: ThemeData(
+      //   primarySwatch: Colors.blue,
+      //   visualDensity: VisualDensity.adaptivePlatformDensity,
+      // ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -35,89 +38,39 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  int _currentIndex = 0;
-
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('ListViewApp'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ListimstrutorPage(),
-                  ),
-                );
-              },
-              // onPressed: () {
-              //   Navigator.pushNamed(context, '/novel_design');
-              // },
-            ),
-          ],
-
-        ),
-        body: ListView(
-          children: <Widget>[
-            GestureDetector(
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                  child: Text('Instructor Screen', style: TextStyle(fontSize: 18)),
-                ),
-              ),
-              onTap: (){
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => ListimstrutorPage(),
-                  ),
-                );
-              },
-            ),
-            GestureDetector(
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                  child: Text('Plants Screen', style: TextStyle(fontSize: 18)),
-                ),
-              ),
-              onTap: (){
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => NovelDesign(),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          // backgroundColor: Colors.blue, //背景色変更
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.arrow_forward),
-              title: Text('Home'),
-              backgroundColor: Colors.blue
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.arrow_forward),
-                title: Text('Setting'),
-                backgroundColor: Colors.blue
-            ),
-          ],
-          onTap: (index){
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-        ),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.filter_list, color: Colors.blue),
+              title: Text('HOME')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.filter_list, color: Colors.blue),
+              title: Text('SETTING')),
+        ],
       ),
+      tabBuilder: (context, index) {
+        switch (index) {
+          case 0:
+            return CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                navigationBar: CupertinoNavigationBar(// ページのヘッダ左のアイコン
+                ),
+                child: Home(),
+              );
+            });
+          case 1:
+            return CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                navigationBar: CupertinoNavigationBar(
+                ),
+                child: Setting(),
+              );
+            });
+        }
+      },
     );
   }
 }
