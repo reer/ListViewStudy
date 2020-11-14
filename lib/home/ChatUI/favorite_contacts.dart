794c990b175f5eaaa1ff93fd51f997c3faef0cd1
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-14 20:45:44 +0900

    画像部分の表示完了-更新-

--- lib/home/ChatUI/favorite_contacts.dart
+++ lib/home/ChatUI/favorite_contacts.dart
@@ -9,42 +9,46 @@ class FavoriteContactsScreen extends StatefulWidget {
 class _favoriteContactsScreen extends State<FavoriteContactsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                'favorite Contacts',
-                style: TextStyle(
-                    color: Colors.blueGrey,
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.0),
-              ),
-              IconButton(
-                icon: Icon(
-                  Icons.more_horiz,
+    return Padding(
+      // padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0), //ここでhorizontalを指定した場合、左右の枠が見える形で横スクロールする
+      padding: EdgeInsets.symmetric(vertical: 10.0), //この画面で表示してる全ての要素の感覚を調整
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'favorite Contacts',
+                  style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0),
                 ),
-                iconSize: 30.0,
-                color: Colors.blueGrey,
-                onPressed: () {},
-              )
-            ],
+                IconButton(
+                  icon: Icon(
+                    Icons.more_horiz,
+                  ),
+                  iconSize: 30.0,
+                  color: Colors.blueGrey,
+                  onPressed: () {},
+                )
+              ],
+            ),
           ),
-        ),
-        Container(
-          height: 120.0,
-          child: ListView.builder(
-            padding: EdgeInsets.only(left: 10.0), //itemの左側に感覚を空ける
-            scrollDirection: Axis.horizontal, //itemを横並びにする
-            //messageモデルのfavoritesからデータを取得
-            itemCount: favorites.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.only(left: 8.0), //全体的な要素(画像部分)の感覚を空ける
+          Container(
+            height: 120.0,
+            child: ListView.builder(
+              padding: EdgeInsets.only(left: 10.0), //itemの左側に感覚を空ける
+              scrollDirection: Axis.horizontal, //itemを横並びにする
+              //messageモデルのfavoritesからデータを取得
+              itemCount: favorites.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  //全体的な要素(画像部分)の感覚を空ける
                   child: Column(
                     children: <Widget>[
                       //messageモデルのfavoritesから、List<User>でUserモデルにアクセスをし、imageUrlを取得
@@ -57,17 +61,19 @@ class _favoriteContactsScreen extends State<FavoriteContactsScreen> {
                       ),
                       Text(
                         favorites[index].name,
-                        style: TextStyle(color: Colors.blueGrey,
+                        style: TextStyle(
+                            color: Colors.blueGrey,
                             fontSize: 16.0,
                             fontWeight: FontWeight.w600),
                       ),
                     ],
                   ),
-              );
-            },
-          ),
-        )
-      ],
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
