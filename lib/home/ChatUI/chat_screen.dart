39d92feb328a3fbfbf3cca9b1c621f5a3bf4756c
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-15 16:31:47 +0900

    chat画面のappBar作成

--- lib/home/ChatUI/chat_screen.dart
+++ lib/home/ChatUI/chat_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_list_app/models/user_model.dart';
 
-
 class ChatScreen extends StatefulWidget {
-
   final User user;
 
   ChatScreen({this.user});
@@ -12,14 +10,23 @@ class ChatScreen extends StatefulWidget {
   _chatScreen createState() => _chatScreen();
 }
 
-
-
 class _chatScreen extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.user.name),
+        title: Text(
+          widget.user.name,
+          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          )
+        ],
       ),
     );
   }
