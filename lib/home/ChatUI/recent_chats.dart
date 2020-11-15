7510d7a4c273b93c5fa69493dd608974a2b4ca74
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-15 15:07:44 +0900

    NEWの表示:非表示判定追加#2

--- lib/home/ChatUI/recent_chats.dart
+++ lib/home/ChatUI/recent_chats.dart
@@ -99,7 +99,7 @@ class _recentChatsScreen extends State<RecentChatsScreen> {
                             ),
                           ),
                         )
-                            : Text(''),
+                            : SizedBox.shrink(),
                       ],
                     )
                   ],
