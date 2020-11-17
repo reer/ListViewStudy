aaa83d93fc25ba8bf0d1e255cf6bc9380efd52f1
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-17 22:41:43 +0900

    Expandedを追加してチャットテキストを表示

--- lib/home/ChatUI/chat_screen.dart
+++ lib/home/ChatUI/chat_screen.dart
@@ -32,21 +32,23 @@ class _chatScreen extends State<ChatScreen> {
       ),
       body: Column(
         children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(30.0),
-                topLeft: Radius.circular(30.0),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30.0),
+                  topLeft: Radius.circular(30.0),
+                ),
               ),
-            ),
-            child: ListView.builder(
-              itemCount: chats.length,
-              //buildContextにmessageモデルのindexが入ってくる
-              itemBuilder: (BuildContext context, int index) {
-                //buildContextにmessageモデルのindexのtextのみ取得して表示
-                return Text(messages[index].text);
-              },
+              child: ListView.builder(
+                itemCount: chats.length,
+                //buildContextにmessageモデルのindexが入ってくる
+                itemBuilder: (BuildContext context, int index) {
+                  //buildContextにmessageモデルのindexのtextのみ取得して表示
+                  return Text(messages[index].text);
+                },
+              ),
             ),
           ),
         ],
