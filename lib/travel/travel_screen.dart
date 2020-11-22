9e18d1ddde4bb7812a42b9c987389f5bd4bcb697
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-22 23:46:07 +0900

    velocity_xを使用してappBarを作成

--- lib/travel/travel_screen.dart
+++ lib/travel/travel_screen.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-
 class TravelScreen extends StatefulWidget {
   @override
   _travelScreen createState() => _travelScreen();
 }
-
-
 
 class _travelScreen extends State<TravelScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Vx.purple500,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: VStack([
+          VxBox().size(20, 2).white.make(),
+          5.heightBox,
+          VxBox().size(26, 2).white.make(),
+          5.heightBox,
+          VxBox().size(15, 2).white.make(),
+        ]).pOnly(left: 16, top: 16),
+      ),
     );
   }
 }
