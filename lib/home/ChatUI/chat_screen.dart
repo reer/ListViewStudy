b8dc86270472c357fcccd2898da2ce10f9bba0b8
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-22 11:20:19 +0900

    chatScreenのメッセージ表示部分とモデルの更新

--- lib/home/ChatUI/chat_screen.dart
+++ lib/home/ChatUI/chat_screen.dart
@@ -8,52 +8,107 @@ class ChatScreen extends StatefulWidget {
   ChatScreen({this.user});
 
   @override
-  _chatScreen createState() => _chatScreen();
+  _ChatScreenState createState() => _ChatScreenState();
 }
 
-class _chatScreen extends State<ChatScreen> {
+class _ChatScreenState extends State<ChatScreen> {
   _buildMessage(Message message, bool isMe) {
-    return Container(
+    final Container msg = Container(
       margin: isMe
           ? EdgeInsets.only(
-              top: 8.0,
-              bottom: 8.0,
-              left: 80.0,
-            )
+        top: 8.0,
+        bottom: 8.0,
+        left: 80.0,
+      )
           : EdgeInsets.only(
-              top: 8.0,
-              bottom: 8.0,
-            ),
+        top: 8.0,
+        bottom: 8.0,
+      ),
       padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      width: MediaQuery.of(context).size.width * 0.75,
       decoration: BoxDecoration(
-          color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
-          borderRadius: isMe
-              ? BorderRadius.only(
-                  topLeft: Radius.circular(15.0),
-                  bottomLeft: Radius.circular(15.0),
-                )
-              : BorderRadius.only(
-                  topRight: Radius.circular(15.0),
-                  bottomRight: Radius.circular(15.0),
-                )),
+        color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+        borderRadius: isMe
+            ? BorderRadius.only(
+          topLeft: Radius.circular(15.0),
+          bottomLeft: Radius.circular(15.0),
+        )
+            : BorderRadius.only(
+          topRight: Radius.circular(15.0),
+          bottomRight: Radius.circular(15.0),
+        ),
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Text(
             message.time,
             style: TextStyle(
-                color: Colors.blueGrey,
-                fontSize: 15.0,
-                fontWeight: FontWeight.w600),
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
           ),
           SizedBox(height: 8.0),
           Text(
             message.text,
             style: TextStyle(
-                color: Colors.black,
-                fontSize: 15.0,
-                fontWeight: FontWeight.w300),
-          )
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+    if (isMe) {
+      return msg;
+    }
+    return Row(
+      children: <Widget>[
+        msg,
+        IconButton(
+          icon: message.isLiked
+              ? Icon(Icons.favorite)
+              : Icon(Icons.favorite_border),
+          iconSize: 30.0,
+          color: message.isLiked
+              ? Theme.of(context).primaryColor
+              : Colors.blueGrey,
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+
+  _buildMessageComposer() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 70.0,
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.photo),
+            iconSize: 25.0,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
+          Expanded(
+            child: TextField(
+              textCapitalization: TextCapitalization.sentences,
+              onChanged: (value) {},
+              decoration: InputDecoration.collapsed(
+                hintText: 'Send a message...',
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.send),
+            iconSize: 25.0,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
         ],
       ),
     );
@@ -66,50 +121,59 @@ class _chatScreen extends State<ChatScreen> {
       appBar: AppBar(
         title: Text(
           widget.user.name,
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          style: TextStyle(
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-        actions: [
+        elevation: 0.0,
+        actions: <Widget>[
           IconButton(
             icon: Icon(Icons.more_horiz),
             iconSize: 30.0,
             color: Colors.white,
             onPressed: () {},
-          )
+          ),
         ],
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(30.0),
-                  topLeft: Radius.circular(30.0),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
                 ),
-              ),
-              child: ClipRRect(
-                borderRadius: (BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
-                )),
-                child: ListView.builder(
-                  // reverse: true,
-                  padding: EdgeInsets.only(top: 15.0),
-                  itemCount: messages.length,
-                  //buildContextにmessageモデルのindexが入ってくる
-                  itemBuilder: (BuildContext context, int index) {
-                    final Message message = messages[index];
-                    //currentUserとmessageのsenderIdが同じであれば、変数isMeに格納する
-                    final bool isMe = message.sender.id == currentUser.id;
-                    //buildContextにmessageモデルのindexのtextのみ取得して表示
-                    return _buildMessage(message, isMe);
-                  },
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
+                  child: ListView.builder(
+                    //messageの表示する場所を調整
+                    reverse: true,
+                    padding: EdgeInsets.only(top: 15.0),
+                    itemCount: messages.length,
+                    //buildContextにmessageモデルのindexが入ってくる
+                    itemBuilder: (BuildContext context, int index) {
+                      final Message message = messages[index];
+                      //currentUserとmessageのsenderIdが同じであれば、変数isMeに格納する
+                      final bool isMe = message.sender.id == currentUser.id;
+                      //buildContextにmessageモデルのindexのtextのみ取得して表示
+                      return _buildMessage(message, isMe);
+                    },
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            _buildMessageComposer(),
+          ],
+        ),
       ),
     );
   }
--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -2,7 +2,8 @@ import 'package:flutter_list_app/models/user_model.dart';
 
 class Message {
   final User sender;
-  final String time;
+  final String
+  time; // Would usually be type DateTime or Firebase Timestamp in production apps
   final String text;
   final bool isLiked;
   final bool unread;
@@ -16,126 +17,110 @@ class Message {
   });
 }
 
+// YOU - current user
 final User currentUser = User(
   id: 0,
   name: 'Current User',
-  imageUrl: 'assets/images/a.png'
+  imageUrl: 'assets/images/jp1.png',
 );
 
-//Users: 登録ユーザー(?)
-final User test1 = User(
-    id: 0,
-    name: 'Test1',
-    imageUrl: 'assets/images/a.png'
+// USERS
+final User Test1 = User(
+  id: 1,
+  name: 'Test1',
+  imageUrl: 'assets/images/jp2.png',
 );
-final User test2 = User(
-    id: 1,
-    name: 'Test2',
-    imageUrl: 'assets/images/a.png'
+final User Test2 = User(
+  id: 2,
+  name: 'Test2',
+  imageUrl: 'assets/images/jp3.png',
 );
-final User test3 = User(
-    id: 2,
-    name: 'Test3',
-    imageUrl: 'assets/images/a.png'
+final User Test3 = User(
+  id: 3,
+  name: 'Test3',
+  imageUrl: 'assets/images/a.png',
 );
-final User test4 = User(
-    id: 3,
-    name: 'Test4',
-    imageUrl: 'assets/images/a.png'
+final User Test4 = User(
+  id: 4,
+  name: 'Test4',
+  imageUrl: 'assets/images/b.png',
 );
-final User test5 = User(
-    id: 4,
-    name: 'Test5',
-    imageUrl: 'assets/images/a.png'
+final User Test5 = User(
+  id: 5,
+  name: 'Test5',
+  imageUrl: 'assets/images/jp4.png',
 );
-final User test6 = User(
-    id: 5,
-    name: 'Test6',
-    imageUrl: 'assets/images/a.png'
+final User Test6 = User(
+  id: 6,
+  name: 'Test6',
+  imageUrl: 'assets/images/jp5.png',
 );
-final User test7 = User(
-    id: 6,
-    name: 'Test7',
-    imageUrl: 'assets/images/a.png'
-);
-final User test8 = User(
-    id: 7,
-    name: 'Test8',
-    imageUrl: 'assets/images/a.png'
-);
-final User test9 = User(
-    id: 8,
-    name: 'Test9',
-    imageUrl: 'assets/images/a.png'
-);
-final User test10 = User(
-    id: 9,
-    name: 'Test10',
-    imageUrl: 'assets/images/a.png'
-);
-final User test11 = User(
-    id: 10,
-    name: 'Test11',
-    imageUrl: 'assets/images/a.png'
+final User Test7 = User(
+  id: 7,
+  name: 'Test7',
+  imageUrl: 'assets/images/jp6.png',
 );
 
+// FAVORITE CONTACTS
+List<User> favorites = [Test1, Test2, Test3, Test4, Test5];
 
-
-//Likeした人
-List<User> favorites = [test1, test2, test3, test4, test5];
-
-
-
-//Chatしている人
+// EXAMPLE CHATS ON HOME SCREEN
 List<Message> chats = [
   Message(
-    sender: test1,
-      time: '5:00 PM',
-      text: 'こんにちは',
-    isLiked: false,
-    unread: false,
-  ),
-  Message(
-    sender: test2,
-    time: '5:00 PM',
-    text: 'こんにちは!!',
+    sender: Test1,
+    time: '5:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
     isLiked: false,
     unread: true,
   ),
   Message(
-    sender: test3,
-    time: '5:00 PM',
-    text: 'Hey, how\'s it going?',
+    sender: Test2,
+    time: '4:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: Test3,
+    time: '3:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
     isLiked: false,
     unread: false,
   ),
   Message(
-    sender: test4,
-    time: '5:00 PM',
-    text: 'Hey, how\'s it going?',
-    isLiked: true,
+    sender: Test4,
+    time: '2:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
     unread: true,
   ),
   Message(
-    sender: test5,
-    time: '5:00 PM',
-    text: '楽しかったですね',
-    isLiked: true,
+    sender: Test1,
+    time: '1:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
     unread: false,
   ),
   Message(
-    sender: test5,
-    time: '5:00 PM',
-    text: 'Hello!!',
-    isLiked: true,
-    unread: true,
+    sender: Test2,
+    time: '12:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: false,
+  ),
+  Message(
+    sender: Test3,
+    time: '11:30 AM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: false,
   ),
 ];
 
-// チャットのメッセージ
+// EXAMPLE MESSAGES IN CHAT SCREEN
 List<Message> messages = [
   Message(
-    sender: test1,
+    sender: Test3,
     time: '5:30 PM',
     text: 'Hey, how\'s it going? What did you do today?',
     isLiked: true,
@@ -144,54 +129,36 @@ List<Message> messages = [
   Message(
     sender: currentUser,
     time: '4:30 PM',
-    text: 'hogehogeでした！',
+    text: 'Just walked my doge. She was super duper cute. The best pupper!!',
     isLiked: false,
     unread: true,
   ),
   Message(
-    sender: test1,
+    sender: currentUser,
     time: '3:45 PM',
     text: 'How\'s the doggo?',
     isLiked: false,
     unread: true,
   ),
   Message(
-    sender: test1,
+    sender: Test4,
     time: '3:15 PM',
-    text: '食べ物',
+    text: 'All the food',
     isLiked: true,
     unread: true,
   ),
   Message(
     sender: currentUser,
     time: '2:30 PM',
-    text: '食べたことある?',
+    text: 'Nice! What kind of food did you eat?',
     isLiked: false,
     unread: true,
   ),
   Message(
-    sender: test1,
+    sender: Test6,
     time: '2:00 PM',
-    text: '今日食べに行った！',
+    text: 'I ate so much food today.',
     isLiked: false,
     unread: true,
   ),
 ];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
