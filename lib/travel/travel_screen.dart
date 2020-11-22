dd9fcecfd7bdef5ad8617f5219eac669883a30d7
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-22 23:23:22 +0900

    travel画面の作成

---
+++ lib/travel/travel_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+
+class TravelScreen extends StatefulWidget {
+  @override
+  _travelScreen createState() => _travelScreen();
+}
+
+
+
+class _travelScreen extends State<TravelScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
