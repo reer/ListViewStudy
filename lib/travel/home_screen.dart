2745e6eab2282f93a8960f74a1c31b4aaa5e794d
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-23 14:35:54 +0900

    custom_barの作成#3

--- lib/travel/home_screen.dart
+++ lib/travel/home_screen.dart
@@ -1,20 +1,56 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_list_app/utiles/custom_app_bar.dart';
-
+import 'package:flutter_list_app/utiles/palette.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
   _homeScreen createState() => _homeScreen();
 }
 
-
-
 class _homeScreen extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: CustomAppBar(),
+      body: CustomScrollView(
+        physics: ClampingScrollPhysics(),
+        slivers: <Widget>[
+          _buildHeader(screenHeight),
+        ],
+      ),
+    );
+  }
+
+  //AppBarの形を変更している。今回の場合角丸
+  SliverToBoxAdapter _buildHeader(double screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: Palette.primaryColor,
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(40.0),
+              bottomRight: Radius.circular(40.0)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text(
+                  'Travel',
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                CountryDropdown(),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
