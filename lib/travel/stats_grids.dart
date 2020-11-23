2839e70624ed288b2410f6b210dd7d22238a2568
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-23 19:35:28 +0900

    StatsGrids画面_カード表示

--- lib/travel/stats_grids.dart
+++ lib/travel/stats_grids.dart
@@ -3,6 +3,54 @@ import 'package:flutter/material.dart';
 class StatsGrids extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: MediaQuery
+          .of(context)
+          .size
+          .height * 0.25,
+      color: Colors.yellow,
+      child: Column(
+        children: <Widget>[
+          _buildStatCard('Total Class', '1.81 M', Colors.orange),
+        ],
+      ),
+    );
+  }
+
+  Expanded _buildStatCard(String title, String count, MaterialColor color) {
+    return Expanded(
+      child: Row(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.circular(10.0)
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(title,
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w600
+                  ),
+                ),
+                Text(count,
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w600
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
