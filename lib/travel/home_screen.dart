d7249d55d68a6bf330e47188b79b7236fba4dd6a
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-23 12:16:02 +0900

    statsScreenとhomeScreenの追加

---
+++ lib/travel/home_screen.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _homeScreen createState() => _homeScreen();
+}
+
+
+
+class _homeScreen extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/travel/stats_screen.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+
+class StatsScreen extends StatefulWidget {
+  @override
+  _statusScreen createState() => _statusScreen();
+}
+
+
+
+class _statusScreen extends State<StatsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
