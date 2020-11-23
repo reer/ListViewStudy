4607c20d976c320138a13b5eaffe621158e27ed6
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-23 18:51:08 +0900

    カスタムのTabBarを表示

--- lib/travel/stats_screen.dart
+++ lib/travel/stats_screen.dart
@@ -1,7 +1,9 @@
 
+import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_list_app/travel/widgets.dart';
 import 'package:flutter_list_app/utiles/palette.dart';
+import 'package:flutter_list_app/utiles/style.dart';
 
 
 class StatsScreen extends StatefulWidget {
@@ -19,6 +21,58 @@ class _statusScreen extends State<StatsScreen> {
       appBar: CustomAppBar(),//共通化させたAppBarを呼び出す
       body: CustomScrollView(
         physics: ClampingScrollPhysics(),
+        slivers: <Widget>[
+          _buildHeader(),
+          _buildRegionTabBar(),
+        ],
+      ),
+    );
+  }
+
+  SliverPadding _buildHeader(){
+    return SliverPadding(
+      padding: const EdgeInsets.all(20.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          'Statistics',
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+      ),
+    );
+  }
+
+  //Cusotm TabBar
+  SliverToBoxAdapter _buildRegionTabBar() {
+    return SliverToBoxAdapter(
+      child: DefaultTabController(
+        length: 2,
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20.0),
+          height: 50.0,
+          decoration: BoxDecoration(
+            color: Colors.white24,
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+          child: TabBar(
+            indicator: BubbleTabIndicator(
+              tabBarIndicatorSize: TabBarIndicatorSize.tab,
+              indicatorHeight: 40.0,
+              indicatorColor: Colors.white,
+            ),
+            labelStyle: Styles.tabTextStyle,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.white,
+            tabs: <Widget>[
+              Text('My Country'),
+              Text('Global'),
+            ],
+            onTap: (index) {},
+          ),
+        ),
       ),
     );
   }
