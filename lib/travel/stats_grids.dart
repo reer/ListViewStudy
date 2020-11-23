c502c056ec886614c636fc350ac73737eb7d4be6
mht-sarina-ooba <s.ooba@mht-code.com>
2020-11-23 19:43:36 +0900

    StatsGrids画面_カード表示(複数)完了

--- lib/travel/stats_grids.dart
+++ lib/travel/stats_grids.dart
@@ -4,14 +4,26 @@ class StatsGrids extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery
-          .of(context)
-          .size
-          .height * 0.25,
-      color: Colors.yellow,
+      height: MediaQuery.of(context).size.height * 0.25,
       child: Column(
         children: <Widget>[
-          _buildStatCard('Total Class', '1.81 M', Colors.orange),
+          Flexible(
+            child: Row(
+              children: <Widget>[
+                _buildStatCard('Total Class', '1.81 M', Colors.orange),
+                _buildStatCard('Deaths', '105 M', Colors.red),
+              ],
+            ),
+          ),
+          Flexible(
+            child: Row(
+              children: <Widget>[
+                _buildStatCard('Total Class', '1.81 M', Colors.green),
+                _buildStatCard('Deaths', '105 M', Colors.blue),
+                _buildStatCard('Deaths', '105 M', Colors.purple),
+              ],
+            ),
+          )
         ],
       ),
     );
@@ -19,37 +31,31 @@ class StatsGrids extends StatelessWidget {
 
   Expanded _buildStatCard(String title, String count, MaterialColor color) {
     return Expanded(
-      child: Row(
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.all(8.0),
-            padding: const EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
-                color: color,
-                borderRadius: BorderRadius.circular(10.0)
+      child: Container(
+        margin: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.circular(10.0)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              title,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.w600),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(title,
-                  style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.w600
-                  ),
-                ),
-                Text(count,
-                  style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.w600
-                  ),
-                ),
-              ],
+            Text(
+              count,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.w600),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
